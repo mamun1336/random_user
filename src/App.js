c478d924d23a4d3c12667e8c0f87bc77a4687523
mamun1336 <abmamun504@gmail.com>
2021-02-17 22:37:13 +0600

    user phone, added user details

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import './App.css';
 import User from './User/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header/Header';
+import { useState } from 'react';
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const [totalSalary, setTotalSalary] = useState(0);
+  function handleAddUser(user){
+    let newUserData = [...userData,user];
+    setUserData(newUserData);
+    const totalSalary = newUserData.reduce((sum, newUserData) => sum + newUserData.annual_salary,0)
+    setTotalSalary(totalSalary);
+}
+console.log(totalSalary);
+
   return (
     <div className="App">
       <Header></Header>
       <div className = "userInfo">
         <div className = "userDetails container">
-          <User></User>
+          <User handleAddUser = {handleAddUser}></User>
         </div>
         <div className = "cardDetails">
-          <h2>user Added</h2>
+          <h2>user Added: {userData.length}</h2>
+          <h4 style={{color: "red"}} >Total salary: {totalSalary}</h4>
         </div>
       </div>
       
--- src/User/User.js
+++ src/User/User.js
@@ -4,9 +4,9 @@ import fakeData from "../fakeData"
 import { Button } from 'react-bootstrap';
 
 
-const User = () => {
-    console.log(fakeData);
+const User = (props) => {
 
+    
 
     return (
         <div>
@@ -15,9 +15,11 @@ const User = () => {
                 <div className = "userCard">
                     <img src={user.photo} alt="user"/>
                     <h2>User Name: {user.first_name} {user.last_name}</h2>
-                    <h3>Contact Info: {user.email}</h3>
+                    <h3>Contact Info: </h3>
+                    <h3>Email: {user.email}</h3>
+                    <h3>Phone: {user.phone}</h3>
                     <h2>Salary: ${user.annual_salary}</h2>
-                    <Button variant="primary">Add User</Button>
+                    <Button variant="primary" onClick = {() => props.handleAddUser(user)}>Add User</Button>
                 </div>)
             }
             
